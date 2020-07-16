7e7ea757c2e7859992b4d8898d906d16d393ddd2
Xamarsia <xamarsia@gmail.com>
2020-07-16 22:30:31 +0300

    refactor: rewritten procedural code to object-oriented

--- test_coffee.py
+++ test_coffee.py
@@ -5,128 +5,144 @@ disposable_cups = 9
 money = 550
 status = True
 variable = " "
+order = 0
+
+
+class CoffeeMachine:
+    water = 400
+    milk = 540
+    coffee = 120
+    disposable_cups = 9
+    money = 550
+    variable = " "
+    order = 0
+
+
+    def __init__(self, water, milk, coffee, disposable_cups, money):
+        self.water = water
+        self.milk = milk
+        self.coffee = coffee
+        self.disposable_cups = disposable_cups
+        self.money = money
 
 
 
-def coffee_machine_has():
-    print("The coffee machine has:  \n" +
-        str(water) + " of water \n" +
-        str(milk) + " of milk \n" +
-        str(coffee) + " of coffee beans \n" +
-        str(disposable_cups) + " of disposable cups \n" +
-        str(money) + " of money\n")
-    return
+    def remaining(self):   # yes
+        print("The coffee machine has:  \n" +
+            str(water) + " of water \n" +
+            str(milk) + " of milk \n" +
+            str(coffee) + " of coffee beans \n" +
+            str(disposable_cups) + " of disposable cups \n" +
+            str(money) + " of money\n")
+        return
 
 
-def buy():
-    order = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-    if order == '1':
+    def buy(self, order):   # yes
+        self.order = order
+        if self.order == '1':
+            if self.water < 250:
+                self.variable = "water"
+            if self.coffee < 16:
+                self.variable = "coffee"
+            if self.disposable_cups < 1:
+                self.variable = "cups"
+
+            if self.water < 250 or self.coffee < 16 or self.disposable_cups < 1:
+                print("Sorry, not enough {}!".format(self.variable))
+            else:
+                print("I have enough resources, making you a coffee!")
+                global water
+            water = self.water - 250
+            global milk
+            milk = self.milk - 0
+            global coffee
+            coffee = self.coffee - 16
+            global disposable_cups
+            disposable_cups = self.disposable_cups - 1
+            global money
+            money = self.money + 4
+
+        elif self.order == '2':
+            if self.water < 350:
+                self.variable = "water"
+            elif self.milk < 75:
+                self.variable = "milk"
+            elif self.coffee < 16:
+                self.variable = "coffee"
+            elif self.disposable_cups < 1:
+                self.variable = "cups"
+
+            if self.water < 350 or self.milk < 75 or self.coffee < 20 or self.disposable_cups < 1:
+                print("Sorry, not enough {}!".format(self.variable))
+            else:
+                print("I have enough resources, making you a coffee!")
+                water = self.water - 350
+                milk = self.milk - 75
+                coffee = self.coffee - 20
+                disposable_cups = self.disposable_cups - 1
+                money = self.money + 7
+
+        elif self.order == '3':
+            if self.water < 200:
+                self.variable = "water"
+            elif self.milk < 100:
+                self.variable = "milk"
+            elif self.coffee < 12:
+                self.variable = "coffee"
+            elif self.disposable_cups < 1:
+                self.variable = "cups"
+
+            if self.water < 200 or self.milk < 100 or self.coffee < 12 or self.disposable_cups < 1:
+                print("Sorry, not enough {}!".format(self.variable))
+            else:
+                print("I have enough resources, making you a coffee!")
+                water = self.water - 200
+                milk = self.milk - 100
+                coffee = self.coffee - 12
+                disposable_cups = self.disposable_cups - 1
+                money = self.money + 6
+        return
+
+
+    def fill(self, wat, mil, cof, cups):  # yes
+        self.wat = wat
+        self.mil = mil
+        self.cof = cof
+        self.cups =cups
         global water
+        water = self.wat + self.water
         global milk
+        milk = self.mil + self.milk
         global coffee
+        coffee = self.cof + self.coffee
         global disposable_cups
+        disposable_cups = self.cups + self.disposable_cups
+        return
+
+
+    def take(self):   # yes
         global money
-        global variable
-        if water < 250:
-            variable = "water"
-        if coffee < 16:
-            variable = "coffee"
-        if disposable_cups < 1:
-            variable = "cups"
-
-        if water < 250 or coffee < 16 or disposable_cups < 1:
-            print("Sorry, not enough {}!".format(variable))
-        else:
-            print("I have enough resources, making you a coffee!")
-        water = water - 250
-        milk = milk - 0
-        coffee = coffee - 16
-        disposable_cups = disposable_cups - 1
-        money = money + 4
-
-    elif order == '2':
-        if water < 350:
-            variable = "water"
-        elif milk < 75:
-            variable = "milk"
-        elif coffee < 16:
-            variable = "coffee"
-        elif disposable_cups < 1:
-            variable = "cups"
-
-        if water < 350 or milk < 75 or coffee < 20 or disposable_cups < 1:
-            print("Sorry, not enough {}!".format(variable))
-        else:
-            print("I have enough resources, making you a coffee!")
-            water = water - 350
-            milk = milk - 75
-            coffee = coffee - 20
-            disposable_cups = disposable_cups - 1
-            money = money + 7
-
-    elif order == '3':
-        if water < 200:
-            variable = "water"
-        elif milk < 100:
-            variable = "milk"
-        elif coffee < 12:
-            variable = "coffee"
-        elif disposable_cups < 1:
-            variable = "cups"
-
-        if water < 200 or milk < 100 or coffee < 12 or disposable_cups < 1:
-            print("Sorry, not enough {}!".format(variable))
-        else:
-            print("I have enough resources, making you a coffee!")
-            water = water - 200
-            milk = milk - 100
-            coffee = coffee - 12
-            disposable_cups = disposable_cups - 1
-            money = money + 6
-    return
-
-
-def fill():
-    wat = int(input("Write how many ml of water do you want to add:"))
-    mil = int(input("Write how many ml of milk do you want to add:"))
-    cof = int(input("Write how many grams of coffee beans do you want to add:"))
-    cups = int(input("Write how many disposable cups of coffee do you want to add:"))
-    global water
-    water = wat + water
-    global milk
-    milk = mil + milk
-    global coffee
-    coffee = cof + coffee
-    global disposable_cups
-    disposable_cups = cups + disposable_cups
-    return
-
-
-def remaining():
-    coffee_machine_has()
-
-
-def exit():
-    global status
-    status = False
-
-
-def take():
-    global money
-    print("I gave you $" + str(money))
-    money = 0
-    return
+        print("I gave you $" + str(self.money))
+        money = 0
+        return
 
 
 while status:
     option = input("Write action (buy, fill, take, remaining, exit):")
+    coffee_machine = CoffeeMachine(water, milk, coffee, disposable_cups, money)
     if option == 'buy':
-        buy()
+        globals()['order'] = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+        coffee_machine.buy(order)
+        # coffee_machine.machine_has()
     elif option == 'fill':
-        fill()
-    elif option == 'take':
-        take()
+        wat = int(input("Write how many ml of water do you want to add:"))
+        mil = int(input("Write how many ml of milk do you want to add:"))
+        cof = int(input("Write how many grams of coffee beans do you want to add:"))
+        cups = int(input("Write how many disposable cups of coffee do you want to add:"))
+        coffee_machine.fill(wat, mil, cof, cups)
     elif option == 'remaining':
-        remaining()
-    elif option == 'exit':
-        exit()
+        coffee_machine.remaining()
+    elif option == "exit":
+        status = False
+    elif option == 'take':
+        coffee_machine.take()
